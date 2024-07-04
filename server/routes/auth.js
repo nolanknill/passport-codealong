b87173b2e12779cadf2949756e6922a1f7ed3b9a
Nolan Knill <nolanknill@gmail.com>
2024-07-04 11:36:37 -0400

    Adds /auth/profile and /auth/logout endpoints

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -29,5 +29,32 @@ router.get('/success-callback', (req, res) => {
     }
 });
 
+// User profile endpoint that requires authentication
+router.get('/profile', (req, res) => {
+  // Passport stores authenticated user information on `req.user` object.
+  // Comes from done function of `deserializeUser`
+
+  // If `req.user` isn't found send back a 401 Unauthorized response
+  if (req.user === undefined)
+    return res.status(401).json({ message: 'Unauthorized' });
+
+  // If user is currently authenticated, send back user info
+  res.status(200).json(req.user);
+});
+
+// Create a logout endpoint
+router.get('/logout', (req, res) => {
+  // Passport adds the logout method to request, it will end user session
+  req.logout((error) => {
+      // This callback function runs after the logout function
+      if (error) {
+          return res.status(500).json({message: "Server error, please try again later", error: error});
+      }
+      
+      // Redirect the user back to client-side application
+      res.redirect(process.env.CLIENT_URL);
+  });
+});
+
 // Export the router module
 export default router;
